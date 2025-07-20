d6ba141b361af7354e70c0e90d58cd6a12736ea3
Nikhil16012 <118714881+Nikhil16012@users.noreply.github.com>
2025-07-20 19:02:50 +0530

    Update api.js

--- kellton_frontend/src/services/api.js
+++ kellton_frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import toast from 'react-hot-toast';
 
-const API_BASE_URL = 'https://todo-task-smart-backend-1.onrender.com/api';
+
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
